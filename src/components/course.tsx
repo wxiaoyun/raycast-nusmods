3ee3294621c16a3c83495596f7665c9ad194f159
wxiaoyun <w.xy020203@gmail.com>
2025-04-24 16:20:13 +0800

    fix: lint

--- src/components/course.tsx
+++ src/components/course.tsx
@@ -3,14 +3,7 @@ import { showFailureToast, useFetch } from "@raycast/utils";
 import { useCallback } from "react";
 import { currentAcadYear } from "../search-nus-courses";
 import { API_BASE_URL, WEBSITE_BASE_URL } from "../utils/constants";
-import {
-  CourseDetails,
-  CourseDetailsSchema,
-  CourseSummary,
-  Prereq,
-  SemesterData,
-  Timetable,
-} from "../utils/nusmods";
+import { CourseDetails, CourseDetailsSchema, CourseSummary, Prereq, SemesterData, Timetable } from "../utils/nusmods";
 
 const formatWorkload = (workload?: number[]) => {
   if (!workload) return null;
